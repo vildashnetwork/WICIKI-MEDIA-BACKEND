c19b3f3876ac1e633ef27c002c4fd153b91df40c
Li Blissz <liblissz3@gmail.com>
2025-10-07 07:40:48 -0700

    hh

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get(
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
             });
             // ${FRONTEND_URL}
-            return res.redirect(`${frontend}/auth?${token}`);
+            return res.redirect(`${frontend}auth?${token}`);
         } catch (err) {
             console.error("Error setting JWT cookie:", err);
             return res.redirect(`${frontend}/login-failed`);
