76d389c0ba3308815ff450c3d2050747f8eefe3b
Li Blissz <liblissz3@gmail.com>
2025-10-06 22:30:45 -0700

    fronnmn908k9

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ app.use(express.json()); // <-- parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = [FRONTEND_URL, "http://localhost:3000"];
+const allowedOrigins = [FRONTEND_URL, "http://localhost:3000", "https://wicikis.vercel.app"];
 
 app.use(cors({
     origin: function (origin, callback) {
