0a8bedb81c97e20f7783118d43143446b93c9ffa
Li Blissz <liblissz3@gmail.com>
2025-10-14 21:03:19 -0700

    hello

--- models/User/User.js
+++ models/User/User.js
@@ -23,7 +23,26 @@ const userSchema = new mongoose.Schema({
   YearsOfExperience: { type: String },
   SpokenLanguages: { type: [String], default: [] },
   ProgrammingLanguages: { type: [String], default: [] },
-  website: { type: String, default: "" }
+  website: { type: String, default: "" },
+
+  profilevissibility: { type: String, enum: ["public", "private"], default: "public" },
+  twofactorenabled: { type: Boolean, default: false },
+  twofactormethod: { type: String, enum: ["email", "sms"], default: "email" },
+  loginalerts: { type: Boolean, default: true },
+
+
+
+
+
+
+
+
+
+
+  accountstatus: { type: String, enum: ["active", "suspended", "deactivated"], default: "active" },
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 const User = mongoose.model('User', userSchema);
