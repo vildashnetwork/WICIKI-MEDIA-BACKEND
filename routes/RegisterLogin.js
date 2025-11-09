f761ed0dbd1c416cd0272b20e5608dabbe58e260
Li Blissz <liblissz3@gmail.com>
2025-11-09 10:25:09 -0800

    mm

--- routes/RegisterLogin.js
+++ routes/RegisterLogin.js
@@ -88,7 +88,12 @@ router.post("/login", async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
-        const token = generateToken(user._id);
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "15d" }
+        );
+
         res.status(200).json({
             message: "Login successful",
             user: sanitizeUser(user),
