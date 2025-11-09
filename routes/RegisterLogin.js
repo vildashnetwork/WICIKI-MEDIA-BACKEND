b8545569f6a48301aac096894c054f3a88b1f817
Li Blissz <liblissz3@gmail.com>
2025-11-09 10:31:11 -0800

    mm

--- routes/RegisterLogin.js
+++ routes/RegisterLogin.js
@@ -85,7 +85,7 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(404).json({ message: "User not found" });
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
         const token = jwt.sign(
