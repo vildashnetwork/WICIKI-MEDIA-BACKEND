994d6a6cc1815e5ac6883d5c9fa0af85c3ba1234
Li Blissz <liblissz3@gmail.com>
2025-12-21 20:03:12 +0100

    mfmf

--- auth/passport.js
+++ auth/passport.js
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `https://wiciki-media-backend.onrender.com/auth/google/callback`,
+            callbackURL: `https://wiciki-media-backend-ahiu.onrender.com/auth/google/callback`,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
