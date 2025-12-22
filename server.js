6a330d8a5f837a367efc566e83e4b77919f570dc
Li Blissz <liblissz3@gmail.com>
2025-12-22 20:12:25 +0100

    fkmf

--- server.js
+++ server.js
@@ -337,7 +337,8 @@ const allowedOrigins = [
     "http://localhost:5174",
     FRONTEND_URL,
     "https://wicikis.vercel.app",
-    "http://127.0.0.1:3100",
+    "http://localhost:3100",
+    "http://localhost:3000",
     "https://wicikis.onrender.com",
 ];
 
